60fff340f27cd815052fd4594d2bb5768062cac3
harshitha <harshithar4@gmail.com>
2024-08-07 19:52:09 +0530

    what is selenium

---
+++ ex_07082024/Lab_s001.py
@@ -0,0 +1,5 @@
+#Selenium is open source suits(suit- contains multiple things,
+# Selenium contains selenium webdriver,selenium IDE and selenium grid)
+#selenium help to automate browser
+#Selenium Versions:Selenium 3 contains JSON wire protocol
+#Selenium version 4 contains W3C protocol
