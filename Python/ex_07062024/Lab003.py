7f04ad53060e6f1b6a213cfa71396e88185649e0
harshitha <harshithar4@gmail.com>
2024-08-08 23:57:38 +0530

    Python class1

---
+++ Python/ex_07062024/Lab003.py
@@ -0,0 +1,12 @@
+a = 10
+print(a)  # int Data type
+str = "Hello world"
+print(str)  # string data type
+e = True
+print(e)  # boolean
+x = 2.3
+print(x)  # float data type
+c = 10 + 3j
+print(c)
+print(type(c))
+print(id(c))
