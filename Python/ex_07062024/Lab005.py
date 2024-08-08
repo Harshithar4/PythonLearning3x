a21fc1557d2206a723d9575d4a8917c0c6983102
harshitha <harshithar4@gmail.com>
2024-08-09 00:25:26 +0530

    string Data type

---
+++ Python/ex_07062024/Lab005.py
@@ -0,0 +1,4 @@
+user1 = int(input("Enter 1st number: "))
+user2 = int(input("Enter 2nd number: "))
+a = user1 + user2
+print(a)
---
+++ Python/ex_07062024/Lab006.py
@@ -0,0 +1,9 @@
+name = 'Harry'
+print(name)
+name1 = "Potter"
+print(name1)
+name2 = """ Harry potter like to walk,
+he like to walk alone with the dog
+he likes to watch movie
+"""  # if there is a long string we are using """ coates
+print(name2)
